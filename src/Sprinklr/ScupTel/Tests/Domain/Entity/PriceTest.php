08a2da56358ac4264b0a135f3d5e428f9f55cbce
lucasmro <lucasmro@gmail.com>
2016-09-14 12:43:47 -0300

    Add more tests to PriceTest

--- src/Sprinklr/ScupTel/Tests/Domain/Entity/PriceTest.php
+++ src/Sprinklr/ScupTel/Tests/Domain/Entity/PriceTest.php
@@ -18,4 +18,64 @@ class PriceTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($toAreaCode, $price->getToAreaCode());
         $this->assertEquals(1.9, $price->getValuePerMinute());
     }
+
+    public function testCreatePriceShouldReturnFromAreaCode16AndToAreaCode11AndNameSaoPaulo()
+    {
+        $fromAreaCode = AreaCodeData::createAreaCode16AndNameRibeiraoPreto();
+        $toAreaCode = AreaCodeData::createAreaCode11AndNameSaoPaulo();
+
+        $price = PriceData::createPriceFromAreaCode16AndNameRibeiraoPretoToAreaCode11AndNameSaoPaulo();
+
+        $this->assertEquals($fromAreaCode, $price->getFromAreaCode());
+        $this->assertEquals($toAreaCode, $price->getToAreaCode());
+        $this->assertEquals(2.9, $price->getValuePerMinute());
+    }
+
+    public function testCreatePriceShouldReturnFromAreaCode11AndNameSaoPauloToAreaCode17AndNameMirassol()
+    {
+        $fromAreaCode = AreaCodeData::createAreaCode11AndNameSaoPaulo();
+        $toAreaCode = AreaCodeData::createAreaCode17AndNameMirassol();
+
+        $price = PriceData::createPriceFromAreaCode11AndNameSaoPauloToAreaCode17AndNameMirassol();
+
+        $this->assertEquals($fromAreaCode, $price->getFromAreaCode());
+        $this->assertEquals($toAreaCode, $price->getToAreaCode());
+        $this->assertEquals(1.7, $price->getValuePerMinute());
+    }
+
+    public function testCreatePriceShouldReturnFromAreaCode17AndNameMirassolToAreaCode11AndNameSaoPaulo()
+    {
+        $fromAreaCode = AreaCodeData::createAreaCode17AndNameMirassol();
+        $toAreaCode = AreaCodeData::createAreaCode11AndNameSaoPaulo();
+
+        $price = PriceData::createPriceFromAreaCode17AndNameMirassolToAreaCode11AndNameSaoPaulo();
+
+        $this->assertEquals($fromAreaCode, $price->getFromAreaCode());
+        $this->assertEquals($toAreaCode, $price->getToAreaCode());
+        $this->assertEquals(2.7, $price->getValuePerMinute());
+    }
+
+    public function testCreatePriceShouldReturnFromAreaCode11AndNameSaoPauloToAreaCode18AndNameTupiPaulista()
+    {
+        $fromAreaCode = AreaCodeData::createAreaCode11AndNameSaoPaulo();
+        $toAreaCode = AreaCodeData::createAreaCode18AndNameTupiPaulista();
+
+        $price = PriceData::createPriceFromAreaCode11AndNameSaoPauloToAreaCode18AndNameTupiPaulista();
+
+        $this->assertEquals($fromAreaCode, $price->getFromAreaCode());
+        $this->assertEquals($toAreaCode, $price->getToAreaCode());
+        $this->assertEquals(0.9, $price->getValuePerMinute());
+    }
+
+    public function testCreatePriceShouldReturnFromAreaCode18AndNameTupiPaulistaToAreaCode11AndNameSaoPaulo()
+    {
+        $fromAreaCode = AreaCodeData::createAreaCode18AndNameTupiPaulista();
+        $toAreaCode = AreaCodeData::createAreaCode11AndNameSaoPaulo();
+
+        $price = PriceData::createPriceFromAreaCode18AndNameTupiPaulistaToAreaCode11AndNameSaoPaulo();
+
+        $this->assertEquals($fromAreaCode, $price->getFromAreaCode());
+        $this->assertEquals($toAreaCode, $price->getToAreaCode());
+        $this->assertEquals(1.9, $price->getValuePerMinute());
+    }
 }
