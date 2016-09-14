38a3d4644885ccc5b76c6df4f14a05f745088215
lucasmro <lucasmro@gmail.com>
2016-09-14 12:43:27 -0300

    Add more tests to PlanTest

--- src/Sprinklr/ScupTel/Tests/Domain/Entity/PlanTest.php
+++ src/Sprinklr/ScupTel/Tests/Domain/Entity/PlanTest.php
@@ -6,12 +6,30 @@ use Sprinklr\ScupTel\Domain\DataFixture\PlanData;
 
 class PlanTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCreatePlanShouldReturnWithNameAndTimeInMinutesAndAdditionalMinuteRate()
+    public function testCreatePlanShouldCreateValidPlanFaleMais30()
     {
         $plan = PlanData::createPlanFaleMais30();
 
         $this->assertEquals('Fale Mais 30', $plan->getName());
         $this->assertEquals(30, $plan->getTimeInMinutes());
+        $this->assertEquals(0.1, $plan->getAdditionalMinuteRate());
+    }
+
+    public function testCreatePlanShouldCreateValidPlanFaleMais60()
+    {
+        $plan = PlanData::createPlanFaleMais60();
+
+        $this->assertEquals('Fale Mais 60', $plan->getName());
+        $this->assertEquals(60, $plan->getTimeInMinutes());
+        $this->assertEquals(0.1, $plan->getAdditionalMinuteRate());
+    }
+
+    public function testCreatePlanShouldCreateValidPlanFaleMais120()
+    {
+        $plan = PlanData::createPlanFaleMais120();
+
+        $this->assertEquals('Fale Mais 120', $plan->getName());
+        $this->assertEquals(120, $plan->getTimeInMinutes());
         $this->assertEquals(0.1, $plan->getAdditionalMinuteRate());
     }
 }
