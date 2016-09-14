8d8a1c80def063ee65626f893af699340ad6a492
lucasmro <lucasmro@gmail.com>
2016-09-14 12:43:04 -0300

    Add more tests to AreaCodeTest

--- src/Sprinklr/ScupTel/Tests/Domain/Entity/AreaCodeTest.php
+++ src/Sprinklr/ScupTel/Tests/Domain/Entity/AreaCodeTest.php
@@ -6,11 +6,27 @@ use Sprinklr\ScupTel\Domain\DataFixture\AreaCodeData;
 
 class AreaCodeTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCreateAreaCodeShouldReturnWithCodeAndName()
+    public function testCreateShouldReturnAreaCode11AndNameSaoPaulo()
     {
         $areaCode = AreaCodeData::createAreaCode11AndNameSaoPaulo();
 
         $this->assertEquals(11, $areaCode->getCode());
         $this->assertEquals('São Paulo', $areaCode->getName());
+    }
+
+    public function testCreateShouldReturnAreaCode17AndNameMirassol()
+    {
+        $areaCode = AreaCodeData::createAreaCode17AndNameMirassol();
+
+        $this->assertEquals(17, $areaCode->getCode());
+        $this->assertEquals('Mirassol', $areaCode->getName());
+    }
+
+    public function testCreateShouldReturnAreaCode18AndNameTupiPaulista()
+    {
+        $areaCode = AreaCodeData::createAreaCode18AndNameTupiPaulista();
+
+        $this->assertEquals(18, $areaCode->getCode());
+        $this->assertEquals('Tupi Paulista', $areaCode->getName());
     }
 }
