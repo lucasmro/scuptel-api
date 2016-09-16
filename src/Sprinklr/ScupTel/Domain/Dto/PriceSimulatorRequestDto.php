890e72de74a2dd714e8e3c2b27eabfcffcbf5f55
lucasmro <lucasmro@gmail.com>
2016-09-16 08:02:04 -0300

    Add constraints on PriceSimulatorRequestDto

--- src/Sprinklr/ScupTel/Domain/Dto/PriceSimulatorRequestDto.php
+++ src/Sprinklr/ScupTel/Domain/Dto/PriceSimulatorRequestDto.php
@@ -2,11 +2,21 @@
 
 namespace Sprinklr\ScupTel\Domain\Dto;
 
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints as Assert;
+
 class PriceSimulatorRequestDto
 {
     private $fromAreaCode;
     private $toAreaCode;
     private $timeInMinutes;
+
+    static public function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('fromAreaCode', new Assert\NotBlank());
+        $metadata->addPropertyConstraint('toAreaCode', new Assert\NotBlank());
+        $metadata->addPropertyConstraint('timeInMinutes', new Assert\NotBlank());
+    }
 
     /**
      * SimulationRequestDto constructor.
